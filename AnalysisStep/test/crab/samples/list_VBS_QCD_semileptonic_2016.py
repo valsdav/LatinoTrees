00a1f2dee5f1305c40ddd9a73c5abcfb4e5d5df7
Davide Valsecchi <davide.valsecchi@cern.ch>
2019-07-08 10:37:48 +0200

    Added list of samples VBS QCD

---
+++ AnalysisStep/test/crab/samples/list_VBS_QCD_semileptonic_2016.py
@@ -0,0 +1,13 @@
+# Automatically created input for latino tree creation script starting from samples/samples_VBS_QCD_semileptonic_2016.py
+
+outputDirectory = "/store/group/OneLepton/QCD_semileptonic_summer16/LatinoTrees/"
+
+samples['WpToLNu_WmTo2J_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/WplusToLNuWminusTo2JJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_WpToLNu_WmTo2J_QCD/", "stepB"]
+samples['WpTo2J_WmToLNu_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/WplusTo2JWminusToLNuJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_WpTo2J_WmToLNu_QCD/", "stepB"]
+samples['WpToLNu_WpTo2J_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/WplusToLNuWplusTo2JJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_WpToLNu_WpTo2J_QCD/", "stepB"]
+samples['WmToLNu_WmTo2J_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/WminusToLNuWminusTo2JJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_WmToLNu_WmTo2J_QCD/", "stepB"]
+samples['WpToLNu_ZTo2J_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/WplusToLNuZTo2JJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_WpToLNu_ZTo2J_QCD/", "stepB"]
+samples['WpTo2J_ZTo2L_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/WplusTo2JZTo2LJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_WpTo2J_ZTo2L_QCD/", "stepB"]
+samples['WmToLNu_ZTo2J_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/WminusToLNuZTo2JJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_WmToLNu_ZTo2J_QCD/", "stepB"]
+samples['WmTo2J_ZTo2L_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/WminusTo2JZTo2LJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_WmTo2J_ZTo2L_QCD/", "stepB"]
+samples['ZTo2L_ZTo2J_QCD']  = ["/store/group/OneLepton/QCD_semileptonic_summer16/ZTo2LZTo2JJJ_QCD_LO_SM_MJJ100PTJ10_TuneCUETP8M1_13TeV-madgraph-pythia8/crab_ZTo2L_ZTo2J_QCD/", "stepB"]
